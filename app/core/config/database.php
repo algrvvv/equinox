39a56dc994765bb26e4b7b5a5ca7559aace60a06
algrvvv <alexandrgr25@gmail.com>
2024-01-07 15:53:30 +0300

    minor changes config/database.php

--- app/core/config/database.php
+++ app/core/config/database.php
@@ -14,10 +14,10 @@ return [
 
         "pgsql" => [
             "migration_table" => "CREATE TABLE IF NOT EXISTS migrations (
-                id serial primary key not null,
-                migration varchar(255),
-                dbname varchar(255),
-                created_at timestamp default current_timestamp
+                id BIGSERIAL PRIMARY KEY NOT NULL,
+                migration VARCHAR(255),
+                dbname VARCHAR(255),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );"
         ]
     ]
