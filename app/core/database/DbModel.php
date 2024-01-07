fade06949f95971d53e335dc8bdc23747d178678
algrvvv <alexandrgr25@gmail.com>
2024-01-07 15:52:30 +0300

    postgresql optimization and testing (#24)

--- app/core/database/DbModel.php
+++ app/core/database/DbModel.php
@@ -26,7 +26,7 @@ abstract class DbModel extends Model
     {
         $keys = array_keys($attrs);
         $params = array_map(fn($k) => ":$k", $keys);
-        $sql = "INSERT INTO `$this->table` (" . implode(', ', $keys) . ") VALUES (" . implode(', ', $params) . ")";
+        $sql = "INSERT INTO $this->table (" . implode(', ', $keys) . ") VALUES (" . implode(', ', $params) . ")";
         $statement = $this->db->prepare($sql); // подготовка запроса
         foreach ($keys as $key) {
             $statement->bindValue(":$key", $attrs[$key]);
@@ -37,7 +37,7 @@ abstract class DbModel extends Model
 
     public function update(array $values, array $condition)
     {
-        $sql = "UPDATE `$this->table` SET ";
+        $sql = "UPDATE $this->table SET ";
         $keys = array_keys($values);
         $attrs = array_map(fn($attr) => ":$attr", $keys);
 
@@ -45,18 +45,18 @@ abstract class DbModel extends Model
         $value_amount = 1;
         foreach ($keys as $key => $value) {
             if ($value_amount > 1) {
-                $sql .= ", `$value` = $attrs[$key]";
+                $sql .= ", $value = $attrs[$key]";
             } else {
-                $sql .= "`$value` = $attrs[$key]";
+                $sql .= "$value = $attrs[$key]";
             }
             $value_amount++;
         }
 
         foreach ($condition as $key => $value) {
             if ($cond_amount > 1) {
-                $sql .= " AND `$key` = :$key";
+                $sql .= " AND $key = :$key";
             } else {
-                $sql .= " WHERE `$key` = :$key";
+                $sql .= " WHERE $key = :$key";
             }
             $cond_amount++;
         }
@@ -78,13 +78,13 @@ abstract class DbModel extends Model
 
     public function delete(array $condition)
     {
-        $sql = "DELETE FROM `$this->table` WHERE";
+        $sql = "DELETE FROM $this->table WHERE";
         $cond_amount = 1;
         foreach ($condition as $key => $value) {
             if ($cond_amount > 1) {
-                $sql .= " AND `$key` = :$key";
+                $sql .= " AND $key = :$key";
             } else {
-                $sql .= "`$key` = :$key";
+                $sql .= "$key = :$key";
             }
             $cond_amount++;
         }
@@ -111,7 +111,7 @@ abstract class DbModel extends Model
         else
             throw new ReceivingData();
 
-        $this->query = $sql . " FROM `$this->table`";
+        $this->query = $sql . " FROM $this->table";
 
         return $this;
     }
@@ -124,7 +124,7 @@ abstract class DbModel extends Model
         if(empty($fields)) throw new ReceivingData();
 
         $sql = $this->query;
-        if(strlen($this->query) < 1) $sql = "SELECT * FROM `$this->table`";
+        if(strlen($this->query) < 1) $sql = "SELECT * FROM $this->table";
 
         foreach ($fields as $col => $val){
             if (str_contains($sql, 'WHERE')) {
