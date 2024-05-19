3c2e8938747a7ca4c96647e3a802534c42c64877
p4xt3r <alexandrgr25@gmail.com>
2024-05-19 21:11:37 +0300

    created facade.php for work with facade in future

---
+++ app/core/Facades/src/Facade.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Copyright (c) 2024 p4xt3r. All rights reserved.
+ */
+
+namespace Imissher\Equinox\app\core\Facades\src;
+
+class Facade
+{
+    /**
+     * Добавление кастомного фасада
+     *
+     * @todo прописать логику добавления фасада
+     * @param mixed $src
+     * @return void
+     */
+    public function create(mixed $src)
+    {
+        //TODO прописать логику добавления фасада
+    }
+}
